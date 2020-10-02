26af23510069313239ed524d9f70e22ad7437c3c
Carlos Sanchez <carlos.sanchez.1575@gmail.com>
2020-10-01 20:39:30 -0400

    class files

---
+++ class_scripts/bootcamp_scripts/logic.py
@@ -0,0 +1,119 @@
+# # math comparisons
+# print(1 > 0)
+
+# a = 0
+# b = 1
+# c = 1
+
+# #logic operators
+# print(f'is a equal to b?: {a == b}')
+# print(f'is a not equal to b?: {a != b}')
+# print(f'is a greater than b?: {a > b}')
+# print(f'is a lesser than b?: {a < b}')
+# print(f'is b greater than or equal to c?: {b >= c}')
+# print(f'is b lesser than or equal to c?: {b <= c}')
+
+# #integer vs floats
+# print(3 == 3.00000)
+
+# booleans
+# print(True == 1)
+# print(True == 1.00)
+# print(True == 0)
+# print(True > 0)
+# print(False == 0)
+
+#string
+
+#print(True == 'True')
+
+
+# a = 'cat'
+# b = 'cat'
+# c = 'CAT'
+
+# print(a == b)
+# print(a != c)
+# print('cat' > 'bat')
+
+
+#compounding logic (or, and, & not)
+
+#or
+# print(2 < 3 or 4 < 3)
+# print(2 > 3 or 4 > 3)
+# print(2 > 3 or 4 < 3)
+# print(2 < 3 or 4 > 3)
+
+# and
+# print(2 < 3 and 4 < 3)
+# print(2 > 3 and 4 > 3)
+# print(2 > 3 and 4 < 3)
+# print(2 < 3 and 4 > 3)
+
+# not
+
+# print(not 2 > 3)
+# print(not False)
+# print(not True)
+
+# conditionals
+
+# a = 5
+
+# if a > 3:
+# 	# you will see this in your command line
+# 	print('greater than 3!')
+	
+
+# if a == 3:
+# 	# you will not see this in your command line
+# 	print(f'{a} equals 3!')
+# else:
+# 	# you will see this in your command line
+# 	print(f'{a} is not equal to 3!')
+
+
+# a = 2
+
+# if a < 0:
+# 	print('a is a negative number')
+# elif a > 0:
+# 	print('a positve number')
+# else:
+# 	print('this will probably never run')	
+
+
+	
+# multiple ifs
+
+# a = 2
+# b = 30
+
+# if a > 10:
+# 	print('a greater than 10')
+# if b > 10:
+# 	print('b greater than 10')
+
+# a = 10
+
+# if a > 0:
+# 	print('a is postive')
+# 	# second lever of if
+# 	if a > 10:
+# 		print('a is greatr than 10')
+# 	else:
+# 		print('a is not greater than 10')
+
+
+answer = input('Do you want to hear a joke? (press y / n')
+
+# code responds accordingly
+if answer == "y"
+	print("I'm against picketing, but I don't know how to show it.")
+	
+
+
+
+
+
---
+++ class_scripts/bootcamp_scripts/print_exercise.py
@@ -0,0 +1,46 @@
+# this line of code prints out a first line of text
+print('hi, this is your computer speaking')
+# this line of code prints out a second line of text
+print('i can run a lot of python code')
+
+# assigning the variable firstname with 'Carlos'
+firstname = 'Carlos'
+
+# print out contents of the variable
+print(firstname)
+
+# intergers
+print(1+3)
+print(1*3)
+print(1-3)
+
+# addition +
+# subtraction -
+# multiplication *
+# division /
+# exponents **
+
+
+positive_number = 10
+negative_number = -5
+added = positive_number + negative_number
+
+print(added)
+
+# floats
+print(1.1+3.1)
+print(1.5*2)
+print(1-3.736)
+
+# conveting float to interger
+print(int(3.0))
+print(int(5.3))
+
+# converting interger to float
+
+print(float(7))
+print(float(4.5))
+
+
+
+
