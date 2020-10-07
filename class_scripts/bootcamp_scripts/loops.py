29bd0d4a35ce0b82b7016877d893b1d92be428b3
Carlos Sanchez <carlos.sanchez.1575@gmail.com>
2020-10-06 20:48:32 -0400

    new stuff

---
+++ class_scripts/bootcamp_scripts/loops.py
@@ -0,0 +1,34 @@
+# for loop
+
+#for x in y:
+	# do something in here
+
+
+	#groceries = ['apples', 'oranges']
+
+#"for each (fruit) in the list of (groceries)":
+	# do something
+	#block
+	# of 
+	# code
+
+
+
+
+#numbers = [12, 8, 41]
+
+#for num in numbers:
+		#print(num)
+
+
+
+#complicated operations in loops
+
+#lower case strings
+
+lower_list = ['here', 'are', 'my', 'lower', 'case', 'words']
+upper_list = []
+
+for i in  lower_list:
+	upper_list.append(i.upper())
+print(upper_list)
